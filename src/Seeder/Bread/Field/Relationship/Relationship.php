21acd7a00a69daaff888ca2a332350c4ae5e143b
Ezequiel Roldan <ezeroldan@gmail.com>
2020-08-03 23:35:39 -0300

    Campo de Relacion obligatorio

--- src/Seeder/Bread/Field/Relationship/Relationship.php
+++ src/Seeder/Bread/Field/Relationship/Relationship.php
@@ -35,5 +35,14 @@ abstract class Relationship extends Field
 
         $this->setExtra('column', $foreignKey);
         $this->setExtra('label', $label);
+
+        if ($required) {
+            $this->setExtra('validation', [
+                'rule' => 'required',
+                'messages' => [
+                    'required' => 'Es obligatorio cargar el campo :attribute.'
+                ]
+            ]);
+        }
     }
 }
